f582df8800a58cfe3dd5cc4460b00e8e3af3b28d
phansitay <82353942+phansitay@users.noreply.github.com>
2022-07-31 00:12:45 +0700

    ss8

--- Module2/src/ss8_clean_code_and_refactoring/TennisGame.java
+++ Module2/src/ss8_clean_code_and_refactoring/TennisGame.java
@@ -33,11 +33,7 @@ public class TennisGame {
         }
         else if (m_score1>=4 || m_score2>=4)
         {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
+            score = getString(m_score1, m_score2);
         }
         else
         {
@@ -62,6 +58,16 @@ public class TennisGame {
                 }
             }
         }
+        return score;
+    }
+
+    private static String getString(int m_score1, int m_score2) {
+        String score;
+        int minusResult = m_score1 - m_score2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
         return score;
     }
 }
