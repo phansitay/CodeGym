93501c83b9efda85cba767e79354311ba153bc92
phansitay <82353942+phansitay@users.noreply.github.com>
2022-09-19 19:21:35 +0700

    ss2

---
+++ Module3/ss2_Thiết kế và tạo csdl_bài tập/[Bài tập] Xây dựng cơ sở dữ liệu quản lý bán hàng.sql
@@ -0,0 +1,29 @@
+create database QuanLyBanHang;
+use QuanLyBanHang;
+
+create table Customer(
+cID int not null primary key,
+cName nvarchar(20) not null,
+cAge int not null);
+
+create table `Order`(
+oID int not null primary key,
+cID int not null,
+oDate datetime,
+oToTalPrice float,
+foreign key(cID) references Customer(cID));
+
+create table Product(
+pID int not null primary key,
+pName nvarchar(50),
+pPrice float);
+
+create table OrderDetail(
+oID int ,
+pID int,
+odQTY varchar(50),
+foreign key(oID) references `Order`(oID),
+foreign key(pID) references Product(pID));
+
+insert into Customer values(1,N'PHAN SỈ TÂY',21);
+select *FROM Customer;
