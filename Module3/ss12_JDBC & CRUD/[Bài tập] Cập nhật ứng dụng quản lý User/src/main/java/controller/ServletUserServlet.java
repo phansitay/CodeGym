533c76cab09454c6a874765e11605ec628a58e8b
phansitay <82353942+phansitay@users.noreply.github.com>
2022-11-02 21:05:08 +0700

    ss12

---
+++ Module3/ss12_JDBC & CRUD/[Bài tập] Cập nhật ứng dụng quản lý User/src/main/java/controller/ServletUserServlet.java
@@ -0,0 +1,152 @@
+package controller;
+
+import dao.UserDao;
+import model.User;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+@WebServlet(name = "UserServlet", value = "/UserServlet")
+public class ServletUserServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private UserDao userDao;
+
+    public void init() {
+        userDao = new UserDao();
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+
+        try {
+            switch (action) {
+                case "create":
+                    showNewForm(request, response);
+                    break;
+                case "edit":
+                    showEditForm(request, response);
+                    break;
+                case "delete":
+                    deleteUser(request, response);
+                    break;
+                default:
+                    listUser(request, response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void listUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<User> listUser = userDao.selectAllUsers();
+        request.setAttribute("listUser", listUser);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void deleteUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, SQLException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        userDao.deleteUser(id);
+
+        List<User> listUser = userDao.selectAllUsers();
+        request.setAttribute("listUser", listUser);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        User existingUser = userDao.selectUser(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("edit.jsp");
+        request.setAttribute("user", existingUser);
+        dispatcher.forward(request, response);
+    }
+
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("create.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String action = request.getParameter("action");
+        if (action == null) {
+            action = "";
+        }
+        try {
+            switch (action) {
+                case "create":
+                    insertUser(request, response);
+                    break;
+                case "edit":
+                    updateUser(request, response);
+                    break;
+                case "search":
+                    searchUser(request,response);
+                    break;
+                case "sort":
+                    sortUser(request,response);
+                    break;
+            }
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+
+    private void sortUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        List<User> listUser = userDao.sortUser();
+        request.setAttribute("listUser",listUser);
+        RequestDispatcher dispatcher= request.getRequestDispatcher("list.jsp");
+        dispatcher.forward(request,response);
+    }
+
+    private void searchUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        String country=request.getParameter("country");
+        List<User> listUser = userDao.searchUser(country);
+        request.setAttribute("listUser", listUser);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("list.jsp");
+        dispatcher.forward(request, response);
+
+    }
+
+    private void updateUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+
+        User book = new User(id, name, email, country);
+        boolean check = userDao.updateUser(book);
+        String mess="Cap nhat khong thanh cong";
+        if(check){
+            mess="Cap nhat thanh cong !";
+        }
+        request.setAttribute("mess",mess);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("edit.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    private void insertUser(HttpServletRequest request, HttpServletResponse response) throws SQLException, ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String country = request.getParameter("country");
+        User newUser = new User(name, email, country);
+        boolean check=userDao.insertUser(newUser);
+        String mess;
+        mess="Them that bai";
+        if(check){
+            mess="Them moi thanh cong !";
+        }
+        request.setAttribute("mess",mess);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("create.jsp");
+        dispatcher.forward(request, response);
+    }
+}
--- apache-tomcat-9.0.68/webapps/ROOT/index.jsp
+++ apache-tomcat-9.0.68/webapps/ROOT/index.jsp
@@ -1,18 +1,17 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: Admin
-  Date: 24/10/2022
-  Time: 6:01 PM
+  Date: 31/10/2022
+  Time: 6:21 PM
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ taglib prefix="C" uri="http://java.sun.com/jsp/jstl/core" %>
 <html>
   <head>
     <title>$Title$</title>
   </head>
   <body>
-  <C:redirect url="/ProductServlet"></C:redirect>
+  <c:redirect url="UserServlet"></c:redirect>
   </body>
 </html>
